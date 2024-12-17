bd5e2774dfc390e517704424bad9a3e87f81615d
Victor Dias <victordiaspereira53@gmail.com>
2024-12-17 12:30:13 -0300

    REFACTOR: MELHORIAS EM ALGUMAS FUNCIONALIDADES DE ACORDO OS ULTIMOS COMMITS

--- routes/api.php
+++ routes/api.php
@@ -3,32 +3,37 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\StripeController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-Route::group(['prefix' => 'users'], function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::post('/', [UserController::class, 'store']);
-    Route::get('/{id}', [UserController::class, 'show']);
-    Route::delete('/{id}', [UserController::class, 'destroy']);
-});
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('/{id}', [UserController::class, 'show'])->name('users.show')->whereUuid('id');
+        Route::put('/{id}', [UserController::class, 'update'])->name('users.update')->whereUuid('id');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy')->whereUuid('id');
+    });
 
-Route::group(['prefix' => 'employees'], function () {
-    Route::get('/', [EmployeeController::class, 'index']);
-    Route::post('/', [EmployeeController::class, 'store']);
-    Route::get('/{id}', [EmployeeController::class, 'show']);
-    Route::delete('/{id}', [EmployeeController::class, 'destroy']);
-});
+    Route::group(['prefix' => 'employees'], function () {
+        Route::get('/', [EmployeeController::class, 'index']);
+        Route::post('/', [EmployeeController::class, 'store']);
+        Route::get('/{id}', [EmployeeController::class, 'show']);
+        Route::delete('/{id}', [EmployeeController::class, 'destroy']);
+    });
 
-Route::middleware('auth')->group(function () {
     Route::post('/create-checkout-session', [StripeController::class, 'createCheckoutSession']);
     Route::get('/checkout-success', [StripeController::class, 'checkoutSuccess'])->name('checkout.success');
     Route::get('/checkout-cancel', [StripeController::class, 'checkoutCancel'])->name('checkout.cancel');
+
+    Route::post('/logout/{id}', [AuthController::class, 'logout']);
+    Route::post('/logout-all', [AuthController::class, 'logoutFromAllDevices']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    Route::get('/sessions', [AuthController::class, 'sessions']);
 });
+
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
